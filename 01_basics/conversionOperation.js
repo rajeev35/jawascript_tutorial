42c193e73bd818951e1e99d6abb8adf8a3af77c9
Rajeev Kumar <92223076+rajeev35@users.noreply.github.com>
2023-11-11 09:32:55 +0000

    conversion of Data Types

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,20 @@
+let score = null;
+
+console.log(typeof score)
+console.log(typeof(score));
+
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33qwe". => NAN= NOT A NUMBER 
+let isLogged = 1;
+let booleanIsLoggedin = Boolean(isLogged)
+//console.log(booleanIsLoggedin)
+
+// 1 =>true 0=>false
+let someNumber = 33;
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
