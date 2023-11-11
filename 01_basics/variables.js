928f9a44d662b7747554800e195f69f118a6835f
Rajeev Kumar <92223076+rajeev35@users.noreply.github.com>
2023-11-11 08:56:03 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,17 @@
+const accountId = 14533
+let accountEmail = "rajeevkumarq400@gmail.com"
+var accountPassword = "123456"
+accountCity = "Jaipur"
+let acccountState;
+
+//accountId = 2 -- not allowed
+console.log(accountId);
+
+accountEmail = "rajeevkumarq400@gmail.com"
+accountPassword = 1234
+accountCity = "Bettiah"
+
+console.table([accountEmail,accountPassword,accountCity,acccountState])
+
+
+
