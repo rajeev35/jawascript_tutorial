231eba4613e9dfb4f3f9b607c6df65aff76fd872
Rajeev Kumar <92223076+rajeev35@users.noreply.github.com>
2023-11-11 08:55:14 +0000

    dataTypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,23 @@
+"use strict" // treat all js code as a new version
+// alert(3+3) //we are using node js not browser
+
+console.log(3+3);
+console.log("Rajeev Momos")
+
+let name = "Rajeev Momos"
+let age = 20;
+let isLoggedin = false
+let state;
+
+// Data Types
+
+//number => 2 to power of 53
+//bigint
+//string => ""
+
+//boolean => true/false
+//null =>standalone value
+//symbol => unique
+
+// object
+console.log(typeof null);
