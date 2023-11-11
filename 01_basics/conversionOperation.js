f1d4a2347fd9999be677dd37e0634c71cfe96b41
Rajeev Kumar <92223076+rajeev35@users.noreply.github.com>
2023-11-11 21:14:00 +0000

    conversion of data types

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -16,5 +16,26 @@ let booleanIsLoggedin = Boolean(isLogged)
 // 1 =>true 0=>false
 let someNumber = 33;
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+
+// ********************Operation********************
+
+
+// let valu = 3;
+// let negValue = -value
+// console.log(negValue)
+
+console.log(2+2)
+console.log(2**2);  // power of 2 
+
+let str1 ="Rajeev";
+let str2 =" Momos";
+let str3  = str1+str2;
+
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter)
