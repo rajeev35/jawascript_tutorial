69df59b3f6414dff90eb56f37fae076093a72243
Rajeev Kumar <92223076+rajeev35@users.noreply.github.com>
2023-11-11 21:49:10 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,42 @@
+ //DataTypes 
+
+ // primitive
+
+ // 7 types : string,number,boolean,null undefined,symbol
+
+ // reference(Non Primitive)
+
+ // Array,Object,Function
+
+
+const score = 200;
+const scoreValue = 200.2;
+const isLoggedin = false
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id===anotherId)
+
+const bigNumber = 123456789543210987667890n
+// javascript is a dynamically type language because  we need never defined the datatypes for the variable.
+
+
+
+const heros = ["shaktiman", "naagraj", "doga"]
+
+let myObj = {
+    name: "Rajeev Momos",
+    age: 20,
+    city: "Noida",
+
+}
+
+
+ const myFunction = function () {
+    console.log("HELLO RAJEEV")
+
+}
+
+
