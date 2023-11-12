b5990ba2e18512fa6d8c0d3b677232ca955ac6d6
Rajeev Kumar <92223076+rajeev35@users.noreply.github.com>
2023-11-12 09:21:45 +0000

    data types summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -39,4 +39,39 @@ let myObj = {
 
 }
 
+//******************************************************************************
+
+//Memory stack and heap.
+
+
+let myYoutubename = "Golu vlogs"
+
+let anothername = myYoutubename
+
+anothername = "chhotadymond"
+
+console.log(myYoutubename)
+
+console.log(anothername);
+
+
+let userOne = {
+    email: "rajeevkumarq400@gmail.com",
+    upi: "user@upi",
+
+
+
+}
+
+
+let userTwo = userOne;
+userTwo.email = "rajeevkumarq400@gmail.com";       //object ko access krne ke liye dot ka use kr rhe hai 
+console.log(userOne.email)
+console.log(userTwo.email)
+
+// primitive data types  stack. ke ander jate hai.   or.  non primitive data types 
+// heap ke ander jate hai 
+// copy lena ho to stack and reference lena ho to heap.
+
+
 
