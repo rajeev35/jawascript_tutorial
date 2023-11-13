14d385fa97aa43bb4ac018ce46e5a4df6a58cffa
Rajeev Kumar <92223076+rajeev35@users.noreply.github.com>
2023-11-13 19:39:17 +0000

    Muth and Num

---
+++ 01_basics/nums-math.js
@@ -0,0 +1,34 @@
+const score = 100;
+console.log(score)
+
+const balance = new Number(400)
+console.log(balance)
+
+// console.log(balance.toString())
+
+
+const otherNumber = 23.3434
+// console.log(otherNumber.toPrecision(3))
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'))
+
+//+++++++++++++++++++++++++++++++++++++++++++++    MATHS.    +++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+console.log(Math)
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6))
+
+// console.log(Math.min(4,5,7,3,7))
+
+// console.log(Math.max(5,5,6,89,7))
+
+console.log(Math.round()*10);
+
+console.log(Math.floor(Math.random()*10+1));
+
+const min =10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max-min+1)+min))
