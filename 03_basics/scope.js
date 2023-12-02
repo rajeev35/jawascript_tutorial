e515fa223a2f37565c37066a1b67a88085d2d38c
Rajeev Kumar <92223076+rajeev35@users.noreply.github.com>
2023-12-02 20:07:53 +0000

    scope concept

---
+++ 03_basics/scope.js
@@ -0,0 +1,73 @@
+// let a  = 7;
+// const b =40;
+
+// var c = 30;
+
+let a = 12;
+
+if(true){
+let a  = 7;
+const b =40;
+
+var c = 30;
+
+
+}
+
+
+// for(let  i =0; i<Array.length;i++){
+
+//     const element = array[i];
+
+// }
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username = "Rajeev"
+
+    function two(){
+        const website = "Youtube"
+        // console.log(username)
+    }
+
+    // console.log(website);
+
+    two()
+}
+
+one()
+
+
+if(true){
+    const username = "Rajeev";
+    if(username === "Rajeev"){
+        const website = "Youtube";
+        console.log(username+website)
+    }
+
+    // console.log(username);
+}
+
+// console.log(username)
+
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++. interesting  ++++++++++++++++++++++++++++++++++++++++++
+
+
+function addone(num){
+    return num+1;
+}
+
+console.log(addone(5));
+
+const addTwo = function(num){
+    return num + 2;
+
+}
+
+addTwo(5)
