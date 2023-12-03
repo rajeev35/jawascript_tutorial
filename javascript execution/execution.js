b7a861c3b11cac0d5e0ad7113138ae86603d84a5
Rajeev Kumar <92223076+rajeev35@users.noreply.github.com>
2023-12-03 22:46:06 +0000

    Execution of javascripts

---
+++ javascript execution/execution.js
@@ -0,0 +1,16 @@
+// javascript execution context
+
+
+
+
+
+// Globle execution context
+// function execution context
+
+
+// eval execution context
+
+
+// memory creation face 
+
+// execution face 
