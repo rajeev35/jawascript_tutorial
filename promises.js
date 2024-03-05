53e6b6f1a14a6809a74be2ed6ce9a3c421374023
Rajeev Kumar <92223076+rajeev35@users.noreply.github.com>
2024-03-05 21:14:31 +0000

    Promises whats going on in future like resolve or reject

---
+++ promises.js
@@ -0,0 +1,61 @@
+const promiseOne = new Promise(function(resolve, reject)
+{
+    // DO an async task
+
+    // DB calls, cryptography, network
+    setTimeout(function(){
+        console.log('Async task is complete');
+        resolve()
+    }, 1000)
+})
+
+promiseOne.then(function(){
+    console.log("Promise consumed");
+})
+
+
+new Promise(function(resolve,resect){
+
+    setTimeout(function(){
+        console.log("Async task 2");
+
+        resolve()
+    }, 1000)
+
+}).then(function(){
+    console.log("Async 2 resolved")
+})
+
+
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({username: "Chai", email: "chai@gmail.com"})
+    },1000)
+})
+
+promiseThree.then(function(user){
+
+    console.log(user);
+
+})
+
+const promiseFour = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = true;
+        if(!error){
+            resolve({username: "Chai", email: "chai@gmail.com"})
+        }else{
+            reject('ERROR: sOMETHING WENT WRONG')
+        }
+    },1000)
+})
+
+promiseFour.then((user)=>{
+    console.log(user)
+
+    return user.username
+}).then((username)=>{
+    console.log(username)
+}).catch(function(error){
+    console.log(error)
+}).finally(()=>console.log("The promise is either resolve or reject"))
