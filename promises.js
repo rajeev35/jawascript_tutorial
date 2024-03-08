a64ef84883d85f33f8458a7b16fe04bd83b6869d
Rajeev Kumar <92223076+rajeev35@users.noreply.github.com>
2024-03-08 06:21:11 +0000

    promises in js

--- promises.js
+++ promises.js
@@ -59,3 +59,36 @@ promiseFour.then((user)=>{
 }).catch(function(error){
     console.log(error)
 }).finally(()=>console.log("The promise is either resolve or reject"))
+
+
+// async function getAllUsers(){
+// try{
+//     const response  = await fetch ('https://jsonplaceholder.typicode.com/users')
+
+
+//      const data = await response.json()
+//      console.log(data)
+// }catch(error){
+//     console.log("E: ", error)
+// }
+// }
+
+// getAllUsers()
+
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response) => {
+
+    return response.json()
+
+})
+
+.then((data) =>{
+
+    console.log(data)
+
+})
+.catch((error) => {
+    console.log(error)
+
+})
